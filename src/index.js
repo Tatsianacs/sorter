2d34f432a065b796feb14ffbac0189004dae8819
tania <masiukevich@gmail.com>
2018-09-13 22:45:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-class Sorter {
-  constructor() {
-    // your implementation
-  }
+function Sorter() {
+    this.arr = [];
+    this.length = 0;
+    this.comparator = function sortNumber(a, b) {
+        return a - b;
+    }
+    this.add = function(item) {
+        this.arr.push(item);
+        this.length++;
+    }
+    this.at = function(index){
+        return this.arr[index];
+    }
+    this.toArray = function() {
+        return this.arr.slice(0);
+    }
+    this.sort = function(arrIndexes) {
+        var arrayToSort = [];
+        var i;
+        for (i = 0; i < arrIndexes.length; i++) {
+            arrayToSort.push(this.arr[arrIndexes[i]]);
+        }
+        arrayToSort.sort(this.comparator);
+        for (i = 0; i < arrIndexes.length; i++) {
+            this.arr[arrIndexes[i]] = arrayToSort[i];
+        }
+    }
+    this.setComparator = function(compareFunction) {
+        this.comparator = compareFunction;
+    }
 
-  add(element) {
-    // your implementation
-  }
 
-  at(index) {
-    // your implementation
-  }
 
-  get length() {
-    // your implementation
-  }
 
-  toArray() {
-    // your implementation
-  }
-
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
 }
-
 module.exports = Sorter;
